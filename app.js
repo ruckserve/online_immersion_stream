77a7fa2d401057495fad799ac106788701161502
Paul Dombkowski <pdombkowski@gmail.com>
2014-08-17 19:40:02 -0500

    minor changes to folder structure and npm installed logfmt for heroku

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (app.get('env') === 'development') {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: {}
         });
     });
 }
