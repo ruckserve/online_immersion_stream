bbc6d6b03d6a80cc344a48463317739c537eb28c
Paul Dombkowski <pdombkowski@gmail.com>
2014-08-19 20:54:39 -0500

    removed the io.use method because headers are accessible without it

--- app.js
+++ app.js
@@ -13,17 +13,6 @@ var app = module.exports = express();
 var server = http.createServer(app);
 var socket = require('./routes/socket.js')(db, pubSub);
 
-// io.of('/chatrooms/:chatroomId/').use(function(socket, next) {
-io.use(function(socket, next) {
-  var handshakeData = socket.request;
-  console.log(handshakeData);
-  global.chatroomId = handshakeData.chatroomId;
-  global.userId = handshakeData.userId;
-  // authenticate against apiKey
-  // if (error) stop and stuff;
-  next();
-});
-
 io.sockets.on('connection', socket);
 
 // development error handler
